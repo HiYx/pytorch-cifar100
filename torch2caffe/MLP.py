59caed4550e7bb80cbeb849cef5173c2755d306a
yanyx2legion <meeason@foxmail.com>
2022-06-23 09:59:55 +0800

    MLP.py

---
+++ torch2caffe/MLP.py
@@ -0,0 +1,68 @@
+import torch
+from torchvision.models import resnet
+from pytorch2caffe import pytorch2caffe
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# 定义网络
+
+class MLP(nn.Module):
+    """ Very simple multi-layer perceptron (also called FFN)"""
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 输入图像channel：1；输出channel：6；5x5卷积核
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # 2x2 Max pooling
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # 如果是方阵,则可以只使用一个数字进行定义
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # 除去批处理维度的其他所有维度
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+net = MLP(1,2,3,4)
+print(net)
+
+
+
+name='resnet18'
+#resnet18=resnet.resnet18()
+#resnet18.eval()
+dummy_input=torch.ones([1,1])
+
+
+pytorch2caffe.trans_net(net, dummy_input, name)
+pytorch2caffe.save_prototxt('{}.prototxt'.format(name))
+pytorch2caffe.save_caffemodel('{}.caffemodel'.format(name))
